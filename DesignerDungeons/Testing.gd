d64ac23ab8841c7f0f570c23fbe26f27b29d0227
mavsm <mathias.vsm@gmail.com>
2018-07-17 16:47:01 -0300

    Progresso

--- DesignerDungeons/Testing.gd
+++ DesignerDungeons/Testing.gd
@@ -1,10 +1,22 @@
 extends Node
 
-var Player = load("res://Scenes/Entities/Player.tscn") 
+var Player = load("res://Scenes/Entities/Player.tscn")
+var Enemy = load("res://Scenes/Entities/Enemy.tscn")
 
-var maze = [[0, -1, -1],
-			[-1, 0, -1],
-			[-1, -1, -1]]
+var maze = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+			[1, 2, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+			[1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 3, 1],
+			[1, 0, 0, 0, 0, 0, 0, 3, 1, 0, 0, 1],
+			[1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1]]
+
+
+func offset(num):
+	return (num*64+32)*1.5
 
 func _ready():
 	var pl = Player.instance()
@@ -13,13 +25,23 @@ func _ready():
 	
 	$tiles.scale = Vector2(1.5, 1.5)
 	
-	for i in range(3):
-		for j in range(3):
-			if maze[i][j] == 0:
-				$tiles.set_cell(i, j, 0)
-			else:
+	for j in range(len(maze)):
+		for i in range(len(maze[j])):
+			if maze[j][i] == 1:
 				$tiles.set_cell(i, j, 1)
+			else:
+				$tiles.set_cell(i, j, 0)
+				if maze[j][i] == 2:
+					pl.set_global_position(Vector2(offset(i), offset(j)))
+				elif maze[j][i] == 3:
+					new_enemy(Vector2(offset(i), offset(j)))
 
+
+func new_enemy(pos):
+	var en = Enemy.instance()
+	add_child(en)
+	en.connect("shoot", self, "_on_shoot")
+	en.set_global_position(pos)
 
 func _on_shoot(bullet, pos, dir):
 	var b = bullet.instance()
