dfe9cd97bcd70f3d006c62cc3a96e39ffea07dcd
mavsm <mathias.vsm@gmail.com>
2018-07-17 02:58:21 -0300

    Progresso em nivel sendo montado a partir de matriz

--- DesignerDungeons/Testing.gd
+++ DesignerDungeons/Testing.gd
@@ -1,11 +1,16 @@
 extends Node
 
+var Player = load("res://Scenes/Entities/Player.tscn") 
+
 var maze = [[0, -1, -1],
 			[-1, 0, -1],
 			[-1, -1, -1]]
 
 func _ready():
-
+	var pl = Player.instance()
+	add_child(pl)
+	pl.connect("shoot", self, "_on_shoot")
+	
 	$tiles.scale = Vector2(1.5, 1.5)
 	
 	for i in range(3):
@@ -14,3 +19,9 @@ func _ready():
 				$tiles.set_cell(i, j, 0)
 			else:
 				$tiles.set_cell(i, j, 1)
+
+
+func _on_shoot(bullet, pos, dir):
+	var b = bullet.instance()
+	add_child(b)
+	b.start(pos, dir)
